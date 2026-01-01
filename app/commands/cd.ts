566bafe3f283f06e745189ceae85a93881191e08
Kevin Mueller <kevin.mueller@tuta.com>
2026-01-01 14:15:45 +0100

    refactored directory handling

--- app/commands/cd.ts
+++ app/commands/cd.ts
@@ -4,8 +4,9 @@ import { config } from "../config/config";
 export default function cd(input: string[]) {
     const path = input[1];
 
-    if (!existsSync(path))
-        console.log(`cd: ${path}: No such file or directory`)
-
-    process.chdir(path)
+    if (!existsSync(path)) {
+        console.log(`cd: ${path}: No such file or directory`);
+        return
+    }
+    process.chdir(path);
 }
