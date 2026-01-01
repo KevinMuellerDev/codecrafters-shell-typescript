82ce5fa9c48c80c013b28a5a2e069a91c94b1f16
Kevin Mueller <kevin.mueller@tuta.com>
2026-01-01 14:24:16 +0100

    fix: homedir navigation

--- app/commands/cd.ts
+++ app/commands/cd.ts
@@ -4,11 +4,11 @@ import { homedir } from "node:os";
 export default function cd(input: string[]) {
     const path = input[1];
 
-    if (!existsSync(path)) {
-        console.log(`cd: ${path}: No such file or directory`);
-        return
-    } else if (path === "~") {
+    if (path === "~") {
         process.chdir(homedir())
+        return
+    } else if (!existsSync(path)) {
+        console.log(`cd: ${path}: No such file or directory`);
         return
     }
     process.chdir(path);
