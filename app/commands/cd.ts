43de8c8bad1cdb89166d633c1bdcadccbe93b696
Kevin Mueller <kevin.mueller@tuta.com>
2026-01-01 14:22:25 +0100

    add: homedir navigation

--- app/commands/cd.ts
+++ app/commands/cd.ts
@@ -1,11 +1,14 @@
 import { existsSync } from "node:fs";
-import { config } from "../config/config";
+import { homedir } from "node:os";
 
 export default function cd(input: string[]) {
     const path = input[1];
 
     if (!existsSync(path)) {
         console.log(`cd: ${path}: No such file or directory`);
+        return
+    } else if (path === "~") {
+        process.chdir(homedir())
         return
     }
     process.chdir(path);
